5c83218a0b29bc4197962929b3b3fcff8d5cff32
Pantera (Mad_Daniel) <89838384+Pantera07@users.noreply.github.com>
2023-03-10 05:50:53 +0900

    Update PacketPlayerPositionAndLook.java

--- src/main/java/ua/nanit/limbo/protocol/packets/play/PacketPlayerPositionAndLook.java
+++ src/main/java/ua/nanit/limbo/protocol/packets/play/PacketPlayerPositionAndLook.java
@@ -59,7 +59,7 @@ public class PacketPlayerPositionAndLook implements PacketOut {
             msg.writeVarInt(teleportId);
         }
 
-        if (version.moreOrEqual(Version.V1_17) && version.less(Version.V1_19_4)) {
+        if (version.fromTo(Version.V1_17, Version.V1_19_3)) {
             msg.writeBoolean(false); // Dismount vehicle
         }
     }
